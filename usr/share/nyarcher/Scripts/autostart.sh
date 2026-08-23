215cf207a79fc97809ab718bb5a6f106b2905cd5
TotallyDIO <totallydio@proton.me>
2026-08-23 14:57:25 -0500

    changed autostart.sh

--- usr/share/nyarcher/Scripts/autostart.sh
+++ usr/share/nyarcher/Scripts/autostart.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 LIVEUSER="live"
+chmod -R 777 ~/.config/nyarch
+chmod -R 777 ~/.local/share/gnome-shell/extensions
+xdg-mime default org.gnome.Nautilus.desktop inode/directory
 if [ "$USER" = "$LIVEUSER" ]; then
-  sudo -E calamares
+   # Disable suspension
+   gsettings set org.gnome.desktop.session idle-delay "uint32 0"
+   gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type "nothing"
+   gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type "nothing"
+   sleep 2
+   sh -c "pkexec calamares"
 else
     nyarchtour
     rm -rf ~/.config/autostart/start.desktop
 fi
-
