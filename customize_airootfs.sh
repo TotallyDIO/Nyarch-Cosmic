d3ef7dcdd47e36812346dd9f429a180a3cddcdd6
TotallyDIO <totallydio@proton.me>
2026-08-23 14:55:43 -0500

    changed NyarchCopyKDE to NyarchCopy

--- customize_airootfs.sh
+++ customize_airootfs.sh
@@ -12,6 +12,6 @@ flatpak install -y org.gtk.Gtk3theme.adw-gtk3 org.gtk.Gtk3theme.adw-gtk3-dark
 flatpak --remove mask "org.freedesktop.Platform.GL.nvidia*"
 
 # Apply Nyarch Copy
-wget https://nyarchlinux.moe/NyarchCopyKDE.tar.gz && tar -xvf NyarchCopyKDE.tar.gz && cd NyarchCopyKDE && bash ./apply_airoot.sh && rm -rf NyarchCopy*
+wget https://nyarchlinux.moe/NyarchCopy.tar.gz && tar -xvf NyarchCopy.tar.gz && cd NyarchCopy && sudo bash ./apply_airoot.sh && rm -rf NyarchCopy*
 
 chmod +x /etc/xdg/autostart/autostart.sh
