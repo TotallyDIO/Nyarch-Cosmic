4d48508782a97eac6aa6bc16a7fe20dda37e2174
TotallyDIO <totallydio@proton.me>
2026-08-23 14:57:34 -0500

    damn

--- usr/share/nyarcher/Scripts/autostart.sh
+++ usr/share/nyarcher/Scripts/autostart.sh
@@ -2,13 +2,8 @@
 
 LIVEUSER="live"
 chmod -R 777 ~/.config/nyarch
-chmod -R 777 ~/.local/share/gnome-shell/extensions
-xdg-mime default org.gnome.Nautilus.desktop inode/directory
+chmod -x /usr/share/nyarcher/Scripts/autostart.sh
 if [ "$USER" = "$LIVEUSER" ]; then
-   # Disable suspension
-   gsettings set org.gnome.desktop.session idle-delay "uint32 0"
-   gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type "nothing"
-   gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type "nothing"
    sleep 2
    sh -c "pkexec calamares"
 else
